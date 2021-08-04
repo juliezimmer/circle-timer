608c5b1e478bd26d627721e73e373c51b5f34c2a
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-08-04 17:00:34 -0500

    added fourth optional argument to constructor and code checks for its use

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 class Timer {
-   constructor(durationInput, startButton, pauseButton){
+   constructor(durationInput, startButton, pauseButton, callbacks){
       this.durationInput = durationInput;
       this.startButton = startButton;
       this.pauseButton = pauseButton;
+      
+      // checks to see if callbacks were provided
+      if(callbacks){
+         this.onStart = callbacks.onStart; // saves a reference to the callack passed in
+         this.onTick = callbacks.onTick;
+         this.onComplete = callbacks.onComplete;
+
+      }
       
       // listens for the click on the play button
       // when clicked, call start method
@@ -11,6 +19,10 @@ class Timer {
    }
 
    start = () => {
+      // checks to see if onStart was defined
+      if(this.onStart){
+         this.onStart();
+      }
       this.tick();
       // takes in reference to the function to invoke
       // 2nd parameter is how often to run that function
@@ -23,7 +35,17 @@ class Timer {
    };
 
    tick = () => {
-      this.timeRemaining = this.timeRemaining - 1; 
+      if(this.timeRemaining <= 0) {
+         this.pause();
+         if(this.onComplete){
+            this.onComplete();ed
+         }
+      } else {
+         this.timeRemaining = this.timeRemaining - 1;
+         if(this.onTick){
+            this.onTick();
+         }
+      }
    };
 
    get timeRemaining(){
@@ -40,7 +62,17 @@ const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
 // create new instance of Timer //
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+   onStart(){
+      console.log("Timer Started");
+   },
+   onTick(){
+      console.log("Timer just ticked down");
+   },
+   onComplete(){
+      console.log("Timer is complete");
+   }
+});
 // Timer class automatically sets up the event listener //
 
 
